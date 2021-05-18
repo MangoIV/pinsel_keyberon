f586a316480200b5518fe03c35f345e6dbc879c5
mangoiv <mangoiv@outlook.com>
2021-05-18 17:01:43 +0200

    keymap done

--- src/main.rs
+++ src/main.rs
@@ -125,6 +125,6 @@ pub static LAYERS: keyberon::layout::Layers = &[
         &[k(Escape),   k(F1), k(F2),  k(F3),  k(F4), k(No), k(No), k(No), k(Minus), k(Equal)],
         &[k(CapsLock), k(F5), k(F6),  k(F7),  k(F8), k(LBracket), k(RBracket), k(No), k(Grave), k(Bslash)],
         &[k(LShift),   k(F9), k(F10), k(F11), k(F12),k(No), k(No), k(No), k(No), k(RShift)],
-        &[Trans,    Trans,  Trans,  k(LAlt),  k(SPACE), Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
+        &[Trans,    Trans,  Trans,  k(LAlt),  k(Space), Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
     ],
 ];
